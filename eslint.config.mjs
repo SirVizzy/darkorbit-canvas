da8be22bad095acd989e7014e9c2fdc3c95dc4b1
Martin <viziousdev@gmail.com>
2024-06-30 01:19:08 +0200

    Add prettier and stylistic rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      stylistic: stylisticJs,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
 ];
